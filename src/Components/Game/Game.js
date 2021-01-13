480934aaa58c5f51182515ed3f6d99202449df24
Amrit Raj <amritrazzz@gmail.com>
2021-01-13 15:30:01 +0530

    fixed winner display method to correctly display winner

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -21,24 +21,40 @@ class Game extends Component {
 			this.calculateWinner(array);
 		}
 	}
+	
+	checkInclusion(i,j,k,arr){
+		if(arr[i] !== null && arr[j] !== null && arr[k] !== null){
+			if(arr[i] === arr[j] && arr[j] === arr[k])
+				return true;
+		}
+		return false;
+	}
 
 	calculateWinner = arr => {
-		if(arr[0] === arr[1] && arr[0] === arr[2] && arr[2] === arr[1])
-			this.setState({winner: arr[0]})
-		else if(arr[3] === arr[4] && arr[3] === arr[5] && arr[4] === arr[5])
-			this.setState({winner: arr[3]})
-		else if(arr[6] === arr[7] && arr[6] === arr[8] && arr[7] === arr[8])
-			this.setState({winner: arr[6]})
-		else if(arr[0] === arr[3] && arr[0] === arr[6] && arr[3] === arr[6])
-			this.setState({winner: arr[0]})
-		else if(arr[1] === arr[4] && arr[1] === arr[7] && arr[4] === arr[7])
+		if(this.checkInclusion(1,0,2,arr)){
 			this.setState({winner: arr[1]})
-		else if(arr[2] === arr[5] && arr[2] === arr[8] && arr[5] === arr[8])
-			this.setState({winner: arr[2]})
-		else if(arr[0] === arr[4] && arr[0] === arr[8] && arr[4] === arr[8])
+		}
+		else if(this.checkInclusion(3,4,5,arr)){
+			this.setState({winner: arr[3]})
+		}
+		else if(this.checkInclusion(6,7,8,arr)){
+			this.setState({winner: arr[6]})
+		}
+		else if(this.checkInclusion(0,3,6,arr)){
 			this.setState({winner: arr[0]})
-		else if(arr[2] === arr[4] && arr[2] === arr[6] && arr[4] === arr[6])
+		}
+		else if(this.checkInclusion(1,4,7,arr)){
+			this.setState({winner: arr[1]})
+		}
+		else if(this.checkInclusion(2,5,8,arr)){
 			this.setState({winner: arr[2]})
+		}
+		else if(this.checkInclusion(0,4,8,arr)){
+			this.setState({winner: arr[0]});
+		}
+		else if(this.checkInclusion(2,4,6,arr)){
+			this.setState({winner: arr[2]});
+		}
 	}
 	// (0,1,2) (3,4,5) (6,7,8) (0,3,6) (1,4,7) (2,5,8) (0,4,8) (2,4,6)
 
